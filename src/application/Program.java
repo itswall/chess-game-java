1f9dac977aaf7ee39014704a19159e21f38989ec
Wall <Wallaceknn@gmail.com>
2024-04-20 22:33:43 -0300

    Fix: better treatment to promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -42,7 +42,11 @@ public class Program {
 
                 if(chessMatch.getPromoted() != null){
                     System.out.print("Enter piece for promotion B/R/N/Q: ");
-                    String type = sc.nextLine();
+                    String type = sc.nextLine().toUpperCase();
+                    while (!type.equals("Q") && !type.equals("R") && !type.equals("B") && !type.equals("N")){
+                        System.out.print("Invalid value! Enter piece for promotion B/R/N/Q: ");
+                        type = sc.nextLine().toUpperCase();
+                    }
                     chessMatch.replacePromotedPiece(type);
                 }
             }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -5,7 +5,6 @@ import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.*;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -121,7 +120,7 @@ public class ChessMatch {
             throw new IllegalStateException("There is no piece to be promoted");
         }
         if(!type.equals("Q") && !type.equals("R") && !type.equals("B") && !type.equals("N")){
-            throw new InvalidParameterException("Invalid type for promotion");
+            return promoted;
         }
         Position pos = promoted.getChessPosition().toPosition();
         Piece p = board.removePiece(pos);
